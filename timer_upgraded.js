b1d002cb1a2aa00c23010e7453f4a2fe35e40d81
HlopakA <faka2@ukr.net>
2024-11-05 14:01:18 +0300

    timer_upgraded_ver0.2

--- timer_upgraded.js
+++ timer_upgraded.js
@@ -4,38 +4,60 @@ timerCreator.addEventListener("click", showTimer);
 
 const content = document.getElementById("content");
 
+const timerInputNum = document.getElementById("write-area").value;
+
 let intervalId;
 let timer;
 let pause;
 let reset;
 
-let test = document.getElementById("text-area").value
 
 function showTimer() {
-    if(!timer) {
+    if(!timer && timerInputNum >= 0) {
         timer = document.createElement("p");
-        timer.innerHTML = document.getElementById("text-area").value;
+        timer.classList = "timer"
+        timer.innerHTML = document.getElementById("write-area").value;
         content.append(timer);
+        console.log("1")
 
-
-        intervalId = setInterval(() => {
-            timer.innerHTML--
-        }, 1000)
-
-
+    
         pause = document.createElement("input");
         pause.type = "button";
         pause.value = "Pause";
         pause.addEventListener("click", pauseTimer);
         content.append(pause);
+        console.log("2")
 
         reset = document.createElement("input");
         reset.type = "button";
         reset.value = "Reset";
         content.append(reset);
+        console.log("3")
+
+        if(!intervalId) {
+            intervalId = setInterval(() => {
+                timer.innerHTML--
+                const timerNum = timer.innerHTML
+
+                if(timerNum <= 0) {
+                    alert("THE END");
+                    clearInterval(intervalId);
+                    timer.remove()
+                    pause.remove()
+                    reset.remove()
+
+                    timer = null;
+                    pause = null;
+                    reset = null;
+                }
+                
+            }, 1000)
+        }
+
 
     } else {
-        timer.innerHTML = document.getElementById("text-area").value;
+        console.log(timer)
+        timer.innerHTML = document.getElementById("write-area").value;
     }
 }
 
